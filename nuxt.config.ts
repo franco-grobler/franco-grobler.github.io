8e8ed318a17313498105194377d4333500896d7d
Franco Grobler <franco@grobler.fyi>
2026-01-23 20:59:54 +0200

    chore: formatting
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: "2025-07-15",
+  compatibilityDate: "2025-07-15",
 
-	content: {
-		build: {
-			markdown: {
-				toc: {
-					depth: 3,
-				},
-			},
-		},
-	},
+  content: {
+    build: {
+      markdown: {
+        toc: {
+          depth: 3,
+        },
+      },
+    },
+  },
 
-	css: ["~/assets/css/main.css"],
+  css: ["~/assets/css/main.css"],
 
-	devtools: { enabled: true },
+  devtools: { enabled: true },
 
-	imports: {
-		autoImport: true,
-	},
+  imports: {
+    autoImport: true,
+  },
 
-	modules: [
-		"@nuxt/content",
-		"@nuxt/eslint",
-		"@nuxt/hints",
-		"@nuxt/image",
-		"@nuxt/test-utils",
-		"@nuxt/ui",
-	],
+  modules: [
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/hints",
+    "@nuxt/image",
+    "@nuxt/test-utils",
+    "@nuxt/ui",
+  ],
 
-	nitro: {
-		prerender: {
-			routes: ["/"],
-			crawlLinks: true,
-		},
-	},
+  nitro: {
+    prerender: {
+      routes: ["/"],
+      crawlLinks: true,
+    },
+  },
 });
