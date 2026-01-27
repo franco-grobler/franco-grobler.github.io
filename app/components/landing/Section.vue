446f0ebae7fc9b67135e436015669a4ace33d7b3
Franco Grobler <franco@grobler.fyi>
2026-01-27 09:53:32 +0200

    feat(component): create base section
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

---
+++ app/components/landing/Section.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="border-t border-white/10 py-24">
+    <div>
+      <slot name="title">
+        <h2 class="mb-16 text-3xl font-medium sm:text-4xl">{{ title }}</h2>
+      </slot>
+    </div>
+
+    <slot />
+  </section>
+</template>
+
+<script setup lang="ts">
+  defineProps<{ title: string }>();
+</script>
