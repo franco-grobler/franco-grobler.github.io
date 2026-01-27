acd3ecbd43673b8c6875b2c63d79df5a4ef637b1
Franco Grobler <franco@grobler.fyi>
2026-01-27 09:54:58 +0200

    feat(pages): basic landing page
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,42 @@
 <template>
-  <UPage> Hello </UPage>
+  <UPage class="mt-20">
+    <LandingIntro
+      v-if="intro"
+      :details="intro"
+      class="mb-10"
+    />
+    <LandingSkills
+      v-if="skills"
+      :skills="skills"
+    />
+  </UPage>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import type { IndexCollectionItem } from "@nuxt/content";
+
+  const { data: page } = await useAsyncData("index", () => {
+    return queryCollection("index").first();
+  });
+  if (!page.value) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: "Page not found",
+      fatal: true,
+    });
+  }
+
+  const intro = computed<IndexCollectionItem["intro"] | undefined>(
+    () => page.value?.intro,
+  );
+  const skills = computed<IndexCollectionItem["skills"] | undefined>(
+    () => page.value?.skills,
+  );
+
+  useSeoMeta({
+    title: page.value.seo.title,
+    ogTitle: page.value.seo.title,
+    description: page.value.seo.description,
+    ogDescription: page.value.seo.description,
+  });
+</script>
