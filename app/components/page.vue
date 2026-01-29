88314c784064d316f6203538305560451d8166c5
Franco Grobler <franco@grobler.fyi>
2026-01-29 09:47:31 +0200

    feat(component): add page component
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

---
+++ app/components/page.vue
@@ -0,0 +1,22 @@
+<template>
+  <UPage class="mt-20">
+    <h1 class="mb-6 text-4xl font-medium sm:text-5xl">
+      {{ title }}
+    </h1>
+
+    <p class="text-muted-foreground max-w-3xl text-xl">
+      {{ description }}
+    </p>
+
+    <div class="mt-16">
+      <slot />
+    </div>
+  </UPage>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    title: string;
+    description: string;
+  }>();
+</script>
