13e7a670e824108d1729f137bb56a287bddc0f2c
Franco Grobler <franco@grobler.fyi>
2026-02-09 20:34:55 +0200

    chore: cleanup
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,9 +9,11 @@ export default defineConfig<ConfigOptions>({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: "html",
+  timeout: 60_000,
   use: {
     trace: "on-first-retry",
     nuxt: {
+      build: true,
       rootDir: fileURLToPath(new URL(".", import.meta.url)),
     },
   },
