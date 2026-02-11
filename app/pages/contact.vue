d4d57530868ce2be9bd87416d40e4cb3876bf317
Franco Grobler <franco@grobler.fyi>
2026-02-11 22:24:25 +0200

    chore(pages): cleanup contact page
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,5 +1,11 @@
 <template>
-  <UPage class="mt-20"> Hello </UPage>
+  <Page
+    title="Get in Touch"
+    description="Interested in collaboration, have a technical question, or want to discuss a project? I'd love to hear from you."
+    class="mt-20"
+  >
+    Hello
+  </Page>
 </template>
 
 <script setup lang="ts"></script>
