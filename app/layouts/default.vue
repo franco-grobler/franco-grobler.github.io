6900be31cbf1b7cc75c7e3d7ca1f008089ed884f
Franco Grobler <franco@grobler.fyi>
2026-02-14 15:54:37 +0200

    fix(layout): keep active for child routes
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <UContainer class="px-10 pt-4 sm:px-12 sm:pt-5 md:pt-6 lg:pt-10">
-      <AppHeader :links="navLinks" />
+      <AppHeader :links="links" />
       <slot />
       <AppFooter />
     </UContainer>
@@ -9,5 +9,18 @@
 </template>
 
 <script setup lang="ts">
+  import type { NavigationMenuItem } from "@nuxt/ui";
   import { navLinks } from "~/utils/links";
+
+  const route = useRoute();
+
+  const links = computed<NavigationMenuItem[]>(() =>
+    navLinks.map<NavigationMenuItem>((i) => {
+      const { to } = i;
+      const isActive =
+        to === "/" ? route.path === "/" : route.path.startsWith(to as string);
+      i.active = isActive;
+      return i;
+    }),
+  );
 </script>
