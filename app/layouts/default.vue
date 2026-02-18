a0bc2194860d8888f57d011b4e9d1e7c2d9593be
Franco Grobler <franco@grobler.fyi>
2026-02-18 22:53:41 +0200

    fix(layout): do not modify active route
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <UContainer class="px-10 pt-4 sm:px-12 sm:pt-5 md:pt-6 lg:pt-10">
-      <AppHeader :links="links" />
+      <AppHeader :links="navLinks" />
       <slot />
       <AppFooter />
     </UContainer>
@@ -9,18 +9,5 @@
 </template>
 
 <script setup lang="ts">
-  import type { NavigationMenuItem } from "@nuxt/ui";
   import { navLinks } from "~/utils/links";
-
-  const route = useRoute();
-
-  const links = computed<NavigationMenuItem[]>(() =>
-    navLinks.map<NavigationMenuItem>((i) => {
-      const { to } = i;
-      const isActive =
-        to === "/" ? route.path === "/" : route.path.startsWith(to as string);
-      i.active = isActive;
-      return i;
-    }),
-  );
 </script>
