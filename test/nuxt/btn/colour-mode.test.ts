6510158d9bd7bebccd4df93f98b623d7946e068e
Franco Grobler <franco@grobler.fyi>
2026-02-19 08:44:53 +0200

    tests(nuxt): update tests foro cour mode button
    add with label test
    
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- test/nuxt/btn/colour-mode.test.ts
+++ test/nuxt/btn/colour-mode.test.ts
@@ -3,16 +3,28 @@ import { mountSuspended } from "@nuxt/test-utils/runtime";
 import ColourMode from "~/components/btn/ColourMode.vue";
 
 describe("ColourMode button", () => {
-  beforeEach(() => {
-    if (typeof document !== "undefined") {
-      document.startViewTransition = vi.fn().mockReturnValue({
-        ready: Promise.resolve(),
-      });
-    }
-  });
+	beforeEach(() => {
+		if (typeof document !== "undefined") {
+			document.startViewTransition = vi.fn().mockReturnValue({
+				ready: Promise.resolve(),
+			});
+		}
+	});
 
-  it("renders correctly", async () => {
-    const component = await mountSuspended(ColourMode);
-    expect(component.exists()).toBe(true);
-  });
+	it("renders correctly", async () => {
+		const component = await mountSuspended(ColourMode);
+		expect(component.exists()).toBe(true);
+		// Only contains icon
+		expect(component.findAll("span").length).toBe(1);
+	});
+
+	it("renders correctly with label", async () => {
+		const component = await mountSuspended(ColourMode, {
+			props: {
+				withLabel: true,
+			},
+		});
+		expect(component.exists()).toBe(true);
+		expect(component.findAll("span")[1]?.text()).toContain("Use");
+	});
 });
